db1fa50cfc2c74ab6f310485168e2d23b75e1397
vish9151 <84173784+vishal9151@users.noreply.github.com>
2023-10-15 17:45:06 +0530

    Learn Tailwind and props management

---
+++ 03TailwindProps/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' flex flex-col justify-center items-center'>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-3'>Chai aur Tailwind</h1>
+      <Card name="Vishal" source={viteLogo}/>
+      <Card name="Chai aur React"/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
